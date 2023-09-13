52c3361ec2827318663e1fa00c986176c8448e63
ΣΠΥΡΙΔΩΝ ΜΠΟΥΚΗΣ <spyros.boukis@hotmail.com>
2023-09-13 14:36:02 +0300

    --- SmartHome Tab Updated ui Added online offline status labels with corresponding colors

--- Model/SmartHomeItem.cs
+++ Model/SmartHomeItem.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Resources;
 
 namespace Personal_Assistant.Model
 {
     public class SmartHomeItem 
     {
         public int Type { get; set; } // 1 = lightbulb , 2 = Temperature , 
+        public string Description { get; set; }
+        public string Status { get; set; } //offline or online
 
         public SmartHomeItem()
         {
                 
         }
 
-        public SmartHomeItem(int type)
+        public SmartHomeItem(int type,string _description,string _status)
         {
-                Type = type;
+            Type = type;
+            Description = _description;
+            Status = _status;
         }
     }
 }
--- Tabs/SmartHomeTab.xaml.cs
+++ Tabs/SmartHomeTab.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Windows.Controls.Image;
 
 namespace Personal_Assistant.Tabs
 {
@@ -29,19 +31,62 @@ namespace Personal_Assistant.Tabs
             InitializeComponent();
 
             items = new List<SmartHomeItem>();
-            items.Add(new SmartHomeItem(1));
-            items.Add(new SmartHomeItem(1));
-            items.Add(new SmartHomeItem(2));
+            items.Add(new SmartHomeItem(1,"Κεντρικό Φως","Online"));
+            items.Add(new SmartHomeItem(1,"Λαμπατέρ Σαλονιού", "Online"));
+            items.Add(new SmartHomeItem(2, "Θερμοκρασία Χώρου", "Online"));
 
             items2 = new List<SmartHomeItem>();
-            items2.Add(new SmartHomeItem(1));
-            items2.Add(new SmartHomeItem(2));
-            items2.Add(new SmartHomeItem(1));
+            items2.Add(new SmartHomeItem(1, "Φως κομοδίνου", "Offline"));
+            items2.Add(new SmartHomeItem(2,"Θερμοκρασία Δωματίου", "Offline"));
+            items2.Add(new SmartHomeItem(1,"Κεντρικά φώτα ", "Online"));
 
             livingRoomListView.ItemsSource = items;
 
             BedroomListView.ItemsSource = items2;
 
+        }
+
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Image _image = (Image)sender;
+            var test = _image.Source.ToString();
+            var test1 = 1;
+           
+            if(_image.Source.ToString() == "pack://application:,,,/Resources/Images/SmartHome/bulb_on.png")
+            {
+                _image.Source = new BitmapImage(new Uri("/Resources/Images/SmartHome/bulb_off.png" +
+                "", UriKind.Relative));
+            }
+            else if (_image.Source.ToString() == "pack://application:,,,/Resources/Images/SmartHome/bulb_off.png")
+            {
+                _image.Source = new BitmapImage(new Uri("/Resources/Images/SmartHome/bulb_on.png" +
+                "", UriKind.Relative));
+            }
+            
+        }
+
+        private void livingRoomList_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(livingRoomListView.Visibility == Visibility.Visible) 
+            {
+                livingRoomListView.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                livingRoomListView.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void bedRoomList_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (BedroomListView.Visibility == Visibility.Visible)
+            {
+                BedroomListView.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BedroomListView.Visibility = Visibility.Visible;
+            }
         }
     }
 }
